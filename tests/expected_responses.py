89c92ae653ed88c049ac85086f08ee2a8b5f7e0a
Lisandro Del Real <ldelreal@ine.com>
2022-06-27 15:54:23 -0300

    added initialization options to SNSSignalHook

--- tests/expected_responses.py
+++ tests/expected_responses.py
@@ -1,9 +1,9 @@
-NO_NESTED_FIELDS_DEFAULT_DEPTH = '[{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": 1, "mainChild": 2, "anotherChildren": [1, 2]}}]'
+NO_NESTED_FIELDS_DEFAULT_DEPTH = '{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": 1, "mainChild": 2, "anotherChildren": [1, 2]}}'
 
-NESTED_FIELDS_DEFAULT_DEPTH = '[{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": {"model": "tests.child", "pk": 1, "fields": {"anotherChild": null, "name": "Child", "desc": "Description for child1"}}, "mainChild": 2, "anotherChildren": [1, 2]}}]'
+NESTED_FIELDS_DEFAULT_DEPTH = '{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": {"model": "tests.child", "pk": 1, "fields": {"anotherChild": null, "name": "Child", "desc": "Description for child1"}}, "mainChild": 2, "anotherChildren": [1, 2]}}'
 
-NESTED_FIELDS_DEPTH_1 = '[{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": {"model": "tests.child", "pk": 1, "fields": {"anotherChild": null, "name": "Child", "desc": "Description for child1"}}, "mainChild": 2, "anotherChildren": [1, 2]}}]'
+NESTED_FIELDS_DEPTH_1 = '{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": {"model": "tests.child", "pk": 1, "fields": {"anotherChild": null, "name": "Child", "desc": "Description for child1"}}, "mainChild": 2, "anotherChildren": [1, 2]}}'
 
-NESTED_FIELDS_DEPTH_2 = '[{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": {"model": "tests.child", "pk": 1, "fields": {"anotherChild": null, "name": "Child", "desc": "Description for child1"}}, "mainChild": 2, "anotherChildren": [{"model": "tests.anotherchild", "pk": 1, "fields": {"name": "Another Child 1"}}, {"model": "tests.anotherchild", "pk": 2, "fields": {"name": "Another Child 2"}}]}}]'
+NESTED_FIELDS_DEPTH_2 = '{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": {"model": "tests.child", "pk": 1, "fields": {"anotherChild": null, "name": "Child", "desc": "Description for child1"}}, "mainChild": 2, "anotherChildren": [{"model": "tests.anotherchild", "pk": 1, "fields": {"name": "Another Child 1"}}, {"model": "tests.anotherchild", "pk": 2, "fields": {"name": "Another Child 2"}}]}}'
 
-ARRAY_NESTED_FIELDS_DEPTH_1 = '[{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": 1, "mainChild": 2, "anotherChildren": [{"model": "tests.anotherchild", "pk": 1, "fields": {"name": "Another Child 1"}}, {"model": "tests.anotherchild", "pk": 2, "fields": {"name": "Another Child 2"}}]}}]'
+ARRAY_NESTED_FIELDS_DEPTH_1 = '{"model": "tests.parent", "pk": 1, "fields": {"name": "New Parent", "child": 1, "mainChild": 2, "anotherChildren": [{"model": "tests.anotherchild", "pk": 1, "fields": {"name": "Another Child 1"}}, {"model": "tests.anotherchild", "pk": 2, "fields": {"name": "Another Child 2"}}]}}'
--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -3,8 +3,10 @@ from django.core.serializers import serialize
 from django.core.management import call_command
 from django.db import connection
 from django.test import TestCase
+from signalhooks.hooks import SNSSignalHook
 from signalhooks.models import Parent, Child, AnotherChild
 from expected_responses import *
+import base64
 import pytest
 import factory
 
@@ -27,38 +29,51 @@ class TestSetup(TestCase):
         self.parent.anotherChildren.add(self.ac2)
 
     def test_serializer_with_no_nested_fields_no_depth(self):
-        result = serialize("json.nested", [self.parent])
+        sns_hook = SNSSignalHook(sns_topic_arn="test_topic", serializer= "json.nested")
+        result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
         print(result)
-        self.assertEqual(result, NO_NESTED_FIELDS_DEFAULT_DEPTH)
+        self.assertEqual(base64.b64decode(result['Instance']['StringValue']).decode("utf-8"), NO_NESTED_FIELDS_DEFAULT_DEPTH)
 
     def test_serializer_with_nested_fields_no_depth(self):
-        result = serialize("json.nested", [self.parent], nested_fields=["child"])
+        sns_hook = SNSSignalHook(sns_topic_arn="test_topic", serializer= "json.nested", nested_fields=["child"])
+        result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
+#        result = serialize("json.nested", [self.parent], nested_fields=["child"])
         print(result)
-        self.assertEqual(result, NESTED_FIELDS_DEFAULT_DEPTH)
+        self.assertEqual(base64.b64decode(result['Instance']['StringValue']).decode("utf-8"), NESTED_FIELDS_DEFAULT_DEPTH)
+#       self.assertEqual(result, NESTED_FIELDS_DEFAULT_DEPTH)
 
     def test_serializer_with_nested_fields_depth_1(self):
-        result = serialize(
-            "json.nested", [self.parent], nested_fields=["child"], max_depth=1
-        )
+        sns_hook = SNSSignalHook(sns_topic_arn="test_topic", serializer= "json.nested", nested_fields=["child"], max_depth=1)
+        result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
+#        result = serialize(
+#            "json.nested", [self.parent], nested_fields=["child"], max_depth=1
+#        )
         print(result)
-        self.assertEqual(result, NESTED_FIELDS_DEPTH_1)
+        self.assertEqual(base64.b64decode(result['Instance']['StringValue']).decode("utf-8"), NESTED_FIELDS_DEPTH_1)
+#        self.assertEqual(result, NESTED_FIELDS_DEPTH_1)
 
     def test_serializer_with_nested_fields_depth_2(self):
-        result = serialize(
-            "json.nested",
-            [self.parent],
-            nested_fields=["child", "anotherChildren"],
-            max_depth=2,
-        )
+        sns_hook = SNSSignalHook(sns_topic_arn="test_topic", serializer= "json.nested", nested_fields=["child", "anotherChildren"], max_depth=2)
+        result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
+        # result = serialize(
+        #     "json.nested",
+        #     [self.parent],
+        #     nested_fields=["child", "anotherChildren"],
+        #     max_depth=2,
+        # )
         print(result)
-        self.assertEqual(result, NESTED_FIELDS_DEPTH_2)
+        self.assertEqual(base64.b64decode(result['Instance']['StringValue']).decode("utf-8"), NESTED_FIELDS_DEPTH_2)
+#        self.assertEqual(result, NESTED_FIELDS_DEPTH_2)
 
     def test_serializer_with_array_nested_fields_depth(self):
-        result = serialize(
-            "json.nested", [self.parent], nested_fields=["anotherChildren"]
-        )
-        print(result)
-        self.assertEqual(result, ARRAY_NESTED_FIELDS_DEPTH_1)
+        sns_hook = SNSSignalHook(sns_topic_arn="test_topic", serializer= "json.nested", nested_fields=["anotherChildren"])
+        result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
+        self.assertEqual(base64.b64decode(result['Instance']['StringValue']).decode("utf-8"), ARRAY_NESTED_FIELDS_DEPTH_1)
+        # result = serialize(
+        #     "json.nested", [self.parent], nested_fields=["anotherChildren"]
+        # )
+        # print(result)
+        # self.assertEqual(result, ARRAY_NESTED_FIELDS_DEPTH_1)
 
 
 class AnotherChildFactory(factory.django.DjangoModelFactory):
