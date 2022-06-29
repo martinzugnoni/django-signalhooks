b7591f6bca0e179edad3e28baa86e1d50a2a3485
Lisandro Del Real <ldelreal@ine.com>
2022-06-29 16:57:36 -0300

    Refactored factories,fixture and expected resp

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -3,11 +3,11 @@ from django.core.management import call_command
 from django.db import connection
 from django.test import TestCase
 from signalhooks.hooks import SNSSignalHook
-from tests.models import Parent, Child, AnotherChild
+from test_fixtures import AnotherChildFactory, ChildFactory, ParentFactory
 from expected_responses import *
 import base64
 import pytest
-import factory
+import json
 
 
 @pytest.mark.django_db
@@ -32,10 +32,9 @@ class TestSetup(TestCase):
     def test_serializer_with_no_nested_fields_no_depth(self):
         sns_hook = SNSSignalHook(sns_topic_arn="test_topic", serializer="json.nested")
         result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
-        print(result)
         self.assertEqual(
             base64.b64decode(result["Instance"]["StringValue"]).decode("utf-8"),
-            NO_NESTED_FIELDS_DEFAULT_DEPTH,
+            json.dumps(NO_NESTED_FIELDS_DEFAULT_DEPTH),
         )
 
     def test_serializer_with_nested_fields_no_depth(self):
@@ -45,10 +44,9 @@ class TestSetup(TestCase):
             nested_fields=["child"],
         )
         result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
-        print(result)
         self.assertEqual(
             base64.b64decode(result["Instance"]["StringValue"]).decode("utf-8"),
-            NESTED_FIELDS_DEFAULT_DEPTH,
+            json.dumps(NESTED_FIELDS_DEFAULT_DEPTH),
         )
 
     def test_serializer_with_nested_fields_depth_1(self):
@@ -61,7 +59,7 @@ class TestSetup(TestCase):
         result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
         self.assertEqual(
             base64.b64decode(result["Instance"]["StringValue"]).decode("utf-8"),
-            NESTED_FIELDS_DEPTH_1,
+            json.dumps(NESTED_FIELDS_DEPTH_1),
         )
 
     def test_serializer_with_nested_fields_depth_2(self):
@@ -74,7 +72,7 @@ class TestSetup(TestCase):
         result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
         self.assertEqual(
             base64.b64decode(result["Instance"]["StringValue"]).decode("utf-8"),
-            NESTED_FIELDS_DEPTH_2,
+            json.dumps(NESTED_FIELDS_DEPTH_2),
         )
 
     def test_serializer_with_array_nested_fields_depth(self):
@@ -86,35 +84,5 @@ class TestSetup(TestCase):
         result = sns_hook.get_sns_msg_attributes(instance=self.parent, created=True)
         self.assertEqual(
             base64.b64decode(result["Instance"]["StringValue"]).decode("utf-8"),
-            ARRAY_NESTED_FIELDS_DEPTH_1,
+            json.dumps(ARRAY_NESTED_FIELDS_DEPTH_1),
         )
-
-
-class AnotherChildFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = AnotherChild
-
-    name = "anotherChild"
-
-
-class ChildFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Child
-
-    name = "default name"
-    desc = "default desc"
-
-
-class ParentFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Parent
-
-    name = "Parent Name"
-
-
-@pytest.fixture(autouse=True)
-@pytest.mark.django_db()
-def fixture():
-    call_command("makemigrations", "tests")
-    connection.disable_constraint_checking()
-    call_command("migrate", "tests")
